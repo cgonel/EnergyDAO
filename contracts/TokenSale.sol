55b5ae7321a56857b531bcf852d7d8978a4fa393
Chris Gonel <gonelw@gmail.com>
2022-04-28 11:53:15 -0400

    token sale

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -2,26 +2,26 @@
 
 pragma solidity ^0.8.13;
 
-// import "./Token.sol";
+import "./Token.sol";
 
-// interface ITokenSale {
-//     function buy(uint256) external payable;
-//     function endSale() external;
-// }
 
-// // what price ratio
-// contract TokenSale is ITokenSale, Ownable {
-//     IERC20 public token;
-//     uint256 public price;
+contract TokenSale is Ownable {
+    IERC20 public token;
+    uint256 public price = 1 gwei;
 
-//     constructor(uint256 _price, uint256 _totalSupply){
-//         price = _price;
-//         token = new MightyToken(_totalSupply);
-//     }
+    constructor(uint256 _totalSupply){
+        token = new Token(_totalSupply);
+    }
 
-//     function buy(uint256) external payable {}
+    function buy(uint256 _amount) external payable {
+        // right amount sent
+        // send token
+        // require(msg.value)
+    }
 
-//     function endSale() external onlyOwner {}
-// }
+    function endSale() external onlyOwner {
+        selfdestruct(msg.sender);
+    }
+}
 
-// // 100000000000000000000000
+// 100000000000000000000000
